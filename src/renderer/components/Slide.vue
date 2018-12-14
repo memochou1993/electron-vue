701d93464840f27e678494bcb6bbbf7f68d5bb6f
William <william@fontrip.com>
2018-12-14 16:26:02 +0800

    新的提交

---
+++ src/renderer/components/Slide.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <Transition
+      name="fade"
+      enter-active-class="animated fadeIn"
+      leave-active-class="animated fadeOut"
+    >
+      <img
+        v-show="loaded"
+        :src="url"
+        class="image center"
+        @load="isLoaded"
+      >
+    </Transition>
+    <div v-show="!loaded">
+      <img
+        class="center"
+        src="../assets/loading.gif"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: '',
+      loaded: false
+    }
+  },
+  created () {
+    this.fetch()
+  },
+  methods: {
+    fetch () {
+      setInterval(function () {
+        this.$http.get(this.$store.state.api_url)
+          .then(({ data }) => {
+            this.url = data.url
+          })
+      }.bind(this), 3000)
+    },
+    isLoaded () {
+      this.loaded = true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css');
+
+.container {
+  width: 100%;
+  height: 100%;
+}
+
+.center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.image {
+  animation-duration: 0.25s;
+  height: 100vh;
+}
+</style>
